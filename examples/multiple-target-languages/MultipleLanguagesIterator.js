e3cbdce8254af79464fdd3a88d638258b3a916b1
Ire Adewolu <ireadewolu@gmail.com>
2024-05-16 13:54:57 +0100

    Adding formatting and lint fixes

--- examples/multiple-target-languages/MultipleLanguagesIterator.js
+++ examples/multiple-target-languages/MultipleLanguagesIterator.js
@@ -1,4 +1,3 @@
-
 // Example of translating HTML content into multiple target languages
 import * as deepl from 'deepl-node';
 import 'dotenv/config';
@@ -11,21 +10,23 @@ var languageCodes = ['BG', 'DE', 'IT', 'ES', 'P'];
 // Applying the translator to each language code. Using Promises to iterate through languages in parallel. Await waits for a response before conducting further logic on output.
 let translatePromises = languageCodes.map((code) =>
     translator
-        .translateDocument("index.html", "index_" + code + ".html", "en", code)
+        .translateDocument('index.html', 'index_' + code + '.html', 'en', code)
         .catch((error) => {
             if (error.documentHandle) {
                 const handle = error.documentHandle;
                 console.log(
-                `Document ID: ${handle.documentId}, ` +
-                    `Document key: ${handle.documentKey}` 
-                    + `For Language Code: ` + code
+                    `Document ID: ${handle.documentId}, ` +
+                        `Document key: ${handle.documentKey}` +
+                        `For Language Code: ` +
+                        code,
                 );
-            } 
-            else {
-                console.log(`For Language Code: ` + code + ` error occurred during document upload. ${error}`);
+            } else {
+                console.log(
+                    `For Language Code: ` +
+                        code +
+                        ` error occurred during document upload. ${error}`,
+                );
             }
         }),
 );
 await Promise.all(translatePromises);
-
-
